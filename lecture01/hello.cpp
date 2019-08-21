ad2d635de8617036a8a80cc3f76b884a178a4134
James Tuck <jtuck@ncsu.edu>
2019-08-21 17:23:11 -0400

    Updates to hello.cpp

--- lecture01/hello.cpp
+++ lecture01/hello.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+class Rectangle {
+private:
+  int width;
+  int length;
+public:
+  Rectangle(int w, int l) {
+    width = w;
+    length = l;
+  }
+  int area() { return width * length; }
+};
+
 int main()
 {
-  printf("Where are all the aliens?\n");
+  int x;
+  Rectangle r1(5,10);
+  Rectangle r2(2,3);
+
+  //printf("Where are all the aliens?\n");
+
+  printf("r1 is %d square units\n", r1.area());
+  printf("r2 is %d square units\n", r2.area());
+  
   return 0;
 }
