687c1d4050683b0339311a5dd2502885f0a086f4
James Tuck <jtuck@ncsu.edu>
2019-10-17 09:00:19 -0400

    lecture15 code

---
+++ lecture15/BinaryTree.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <random>
+
+// Class to represent a node of a binary tree, left here so that main will compile
+class BTNode
+{
+public:
+  int data;
+  BTNode *right;
+  BTNode *left;
+
+  BTNode(int d, BTNode *l=nullptr, BTNode *r=nullptr)
+  {
+    data = d;
+    right = r;
+    left = l;
+  }  
+};
+
+class BinarySearchTree {
+protected:
+  // Class to represent a node of a binary tree
+  class BTNode
+  {
+  public:
+    int data;
+    BTNode *right;
+    BTNode *left;
+    
+    BTNode(int d, BTNode *l=nullptr, BTNode *r=nullptr)
+    {
+      data = d;
+      right = r;
+      left = l;
+
+    }
+  };
+
+  BTNode *root;
+  
+public:
+  BinarySearchTree()
+  {
+    root = nullptr;
+  }
+
+  void insert(int data)
+  {
+    BTNode *node = new BTNode(data);
+
+    if (root == nullptr)
+      {
+	root = node;
+      }
+    else
+      {   
+	BTNode * tmp = root;
+	while (tmp != nullptr)
+	  {	    
+	    if (data < tmp->data)
+	      {
+		// data must be on left side
+		if (tmp->left == nullptr)
+		  {
+		    tmp->left = node; // 
+		    return;
+		  }
+		else
+		  tmp = tmp->left;
+	      }
+	    else
+	      {
+		// data must be on right side
+		if (tmp->right == nullptr)
+		  {
+		    tmp->right = node;
+		    return;		    
+		  }
+		else
+		  tmp = tmp->right;
+	      }
+	  }
+      }
+
+  }
+
+  bool search(int data) {
+    BTNode * tmp = root;
+    while (tmp != nullptr)
+      {
+	if (data == tmp->data)
+	  return true;
+	
+	if (data < tmp->data)
+	  tmp = tmp->left;
+	else
+	  tmp = tmp->right;
+      }
+    return false;
+  }
+
+private:
+  void printInorderHelper(BTNode *node)
+  {
+    if (node==nullptr) return;
+    printInorderHelper(node->left);
+    std::cout << node->data << " (";
+    if (node->left) std:: cout << node->left->data;
+    else std::cout << "-";
+    std:: cout << ",";
+    if (node->right) std:: cout << node->right->data;
+    else std::cout << "-";
+    std::cout << ") " ;
+    
+    printInorderHelper(node->right);    
+  }
+
+public:
+  void printInorder()
+  {
+    printInorderHelper(root);
+  }
+  
+};
+
+
+int main()
+{
+  BTNode * root_1 = new BTNode(100, new BTNode(25), new BTNode(150));
+
+  // 100, 150, 200
+
+  BTNode * root_2 = new BTNode(100, nullptr,
+			       new BTNode(150, nullptr,
+					  new BTNode(200)));
+  
+
+  BinarySearchTree b;
+
+  b.insert(100);
+  b.insert(25);
+  b.insert(150);
+
+  b.printInorder();
+  std::cout << std::endl;
+  
+  std::random_device generator;
+  std::uniform_int_distribution<int> dist(0,100);
+
+  BinarySearchTree b2;
+  for(int i=0; i<20; i++)
+    {
+      b2.insert(dist(generator));      
+    }
+
+  b2.printInorder();
+
+  std::cout << std::endl;
+  
+  return 0;
+}
